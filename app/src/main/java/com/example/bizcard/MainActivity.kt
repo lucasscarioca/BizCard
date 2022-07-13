de09aa73c48f21ca69a2a556aa606e003f8012f3
Lucas Carioca <lucascarioca97@gmail.com>
2022-07-13 13:25:18 -0300

    added surface and card composable functions

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CreateBizCard()
                 }
             }
         }
@@ -30,14 +35,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard() {
+    Surface( modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            backgroundColor = Color.White,
+            elevation = 4.dp
+        ) {
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BizCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
